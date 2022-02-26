471c289b72e2e2032b43e7757e74b108099fd72c
Andrey <xxx@yyy.ru>
2022-02-26 23:48:40 +0300

    Home work 11 edition 1

--- main/views.py
+++ main/views.py
@@ -11,4 +11,5 @@ class IndexView(TemplateView):
         context['events'] = Event.objects.all().order_by('-pk')[:3]
         context['reviews'] = Review.objects.all().order_by('-pk')[:3]
         context['main'] = True
+        division_by_zero = 1 / 0
         return context
